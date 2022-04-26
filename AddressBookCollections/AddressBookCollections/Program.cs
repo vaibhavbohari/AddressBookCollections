14e66b0a81772a77c06de026a43bdd9ca00c76e2
vaibhavbohari <vaibhavbohari@gmail.com>
2022-04-26 17:46:42 +0530

    Edit Contact

--- AddressBookCollections/AddressBookCollections/Program.cs
+++ AddressBookCollections/AddressBookCollections/Program.cs
@@ -12,12 +12,8 @@ namespace AddressBookCollections
         //Main  Method / program Entry Point
         static void Main(string[] args)
         {
-
-            //Showing Welcome Message On console.
-            Console.WriteLine("Welcome to Address Book Progam \n");
-
-            AddressBook addressBook = new AddressBook();
-            addressBook.AddContact();
+            AddressBook createAddressBook = new AddressBook();
+            createAddressBook.ReadInput();
             Console.ReadLine();
 
 
